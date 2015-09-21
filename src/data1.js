3d8f628c1788d9212b0169aed3b9a21024ab4141
Nilamben Khacharia <voranilamj@gmail.com>
2015-09-21 15:41:26 -0700

    first

---
+++ src/data1.js
@@ -0,0 +1,123 @@
+facebook_data({
+  "id": "1039482444",
+  "name": "Nilam",
+  "friends": {
+    "data": [
+      {
+			"location" : {
+				"id" : "115440481803904",
+				"name" : "Ahmedabad,Gujarat, India",
+				"latlng" : {
+					"lat" : 23.022505,
+					"lng" : 72.57136209999999
+				}
+			},
+			"sports" : {
+				"id" : "108167272538953",
+				"name" : "Tennis"
+			},
+			"first_name" : "Nirav",
+			"id" : "2544250"
+		}, {
+			"location" : {
+				"id" : "115440481803905",
+				"name" : "Rajkot,Gujarat, India",
+				"latlng" : {
+					"lat" : 22.3038945,
+					"lng" : 70.80215989999999
+				}
+			},
+			"sports" : {
+				"id" : "108167272538953",
+				"name" : "Basketball"
+			},
+			"first_name" : "Kanchan",
+			"id" : "31007302"
+		}, {
+			"location" : {
+				"id" : "115440481803904",
+				"name" : "Surat,Gujarat, India",
+				"latlng" : {
+					"lat" : 21.195,
+					"lng" : 72.81944399999998
+				}
+			},
+			"sports" : {
+				"id" : "108167272538953",
+				"name" : "Baseball"
+			},
+			"first_name" : "Aboli",
+			"id" : "531178878"
+		}, 
+		
+		{
+			"location" : {
+				"id" : "115440481803945",
+				"name" : "mumbai, maharashtra,india",
+				"latlng" : {
+					"lat" : 18.93013,
+					"lng" : 72.8300934
+				}
+			},
+			"sports" : {
+				"id" : "108167272538953",
+				"name" : "Basketball"
+			},
+			"first_name" : "Ashwin",
+			"id" : "31007302"
+		},
+		{
+			"location" : {
+				"id" : "115440481803134",
+				"name" : "kolkata, west bengal, india",
+				"latlng" : {
+					"lat" : 22.5650902,
+					"lng" : 88.3707809
+				}
+			},
+			"sports" : {
+				"id" : "108167272538953",
+				"name" : "Football"
+			},
+			"first_name" : "Vaishnav",
+			"id" : "31007302"
+		},
+		{
+			"location" : {
+				"id" : "115440481803235",
+				"name" : "koraput, odisha, india",
+				"latlng" : {
+					"lat" : 18.8192997,
+					"lng" : 82.7120972
+				}
+			},
+			"sports" : {
+				"id" : "108167272538953",
+				"name" : "Cricket"
+			},
+			"first_name" : "Vaish",
+			"id" : "31007302"
+		},
+		{
+			"location" : {
+				"id" : "115440481803904",
+				"name" : "Uttarpradesh, India",
+				"latlng" : {
+					"lat" : 27.5705886,
+					"lng" : 80.09818689999997
+				}
+			},
+			"sports" : {
+				"id" : "108167272538953",
+				"name" : "Cricket"
+			},
+			"first_name" : "Vishal",
+			"id" : "555344331"
+		}
+    ],
+    "paging": {
+      "next": "https://graph.facebook.com/1039482449/friends?limit=50&fields=location,sports,first_name&offset=50&__after_id=732549681"
+    }
+  }
+}
+)
